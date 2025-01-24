fe03af525a6573ab4434d31408e2be7747b8adc2
Tylor <tylor.9.kid@gmail.com>
2025-01-24 12:30:17 +0300

    Add files via upload

---
+++ Plugins/Ai/turbo.js
@@ -0,0 +1,24 @@
+// XPLOADER BOT by Tylor
+
+const fetch = require('node-fetch');
+
+module.exports = {
+  command: ['turbo'],
+  operate: async ({ Xploader, m, reply, text }) => {
+    if (!text) return reply("*Please ask a question*");
+
+    try {
+      let response = await fetch(`https://api.tioo.eu.org/lepton?text=${encodeURIComponent(text)}`);
+      let data = await response.json();
+
+      if (!data.result) {
+        return reply("*Please try again later or try another command!*");
+      } else {
+        reply(data.result);
+      }
+    } catch (error) {
+      console.error('Error fetching response from Turbo API:', error);
+      reply("An error occurred while fetching the response from Turbo API.");
+    }
+  }
+};
