26b7c7a1bcdea7a60f89cb3bd4c2493a6f5f2e63
Tylor <tylor.9.kid@gmail.com>
2025-02-09 18:53:21 +0300

    Add files via upload

--- Plugins/Audio/smooth.js
+++ Plugins/Audio/smooth.js
@@ -1,23 +1,22 @@
-// XPLOADER BOT by Tylor
 
 const fs = require('fs');
 const { exec } = require('child_process');
-const { getRandom } = require('../../lib/myfunc'); // Correct import path
+const { getRandom } = require('../../lib/myfunc');
 
 module.exports = {
   command: ['smooth'],
-  operate: async ({ Xploader, m, reply, quoted, mime, prefix, command }) => {
+  operate: async ({ Cypher, m, reply, quoted, mime, prefix, command }) => {
     try {
       const set = "-filter:v \"minterpolate='mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=120'\"";
       
       if (/audio/.test(mime)) {
-        let media = await Xploader.downloadAndSaveMediaMessage(quoted);
+        let media = await Cypher.downloadAndSaveMediaMessage(quoted);
         let ran = getRandom(".mp3");
         exec(`ffmpeg -i ${media} ${set} ${ran}`, (err, stderr, stdout) => {
           fs.unlinkSync(media);
           if (err) return reply(err);
           let buff = fs.readFileSync(ran);
-          Xploader.sendMessage(
+          Cypher.sendMessage(
             m.chat,
             { audio: buff, mimetype: "audio/mpeg" },
             { quoted: m }
