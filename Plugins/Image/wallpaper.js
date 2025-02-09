388e5bd73328d673169c74f7e08dd3dd9836682c
Tylor <tylor.9.kid@gmail.com>
2025-02-09 19:04:36 +0300

    Add files via upload

--- Plugins/Image/wallpaper.js
+++ Plugins/Image/wallpaper.js
@@ -1,15 +1,15 @@
-// XPLOADER-BOT by Tylor
+
  const { wallpaper } = require('../../lib/scraper');
 
 module.exports = {
   command: ['wallpaper'],
-  operate: async ({ m, text, Xploader, reply }) => {
+  operate: async ({ m, text, Cypher, reply }) => {
     if (!text) return reply('Enter a search query');
     
     try {
-      let anuwallpep = await wallpaper(text);
-      let result = anuwallpep[Math.floor(Math.random() * anuwallpep.length)];
-      await Xploader.sendMessage(
+      let wall = await wallpaper(text);
+      let result = wall[Math.floor(Math.random() * wall.length)];
+      await Cypher.sendMessage(
         m.chat,
         {
           caption: `TITLE: ${result.title}\nCATEGORY: ${result.type}\nDETAIL: ${result.source}\nMEDIA URL: ${result.image[2] || result.image[1] || result.image[0]}`,
