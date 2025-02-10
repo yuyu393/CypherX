5c728be982ea2ccfd815d3ed6db651ebc44a3e56
Tylor <tylor.9.kid@gmail.com>
2025-02-10 17:05:13 +0300

    Add files via upload

--- Plugins/Other/script.js
+++ Plugins/Other/script.js
@@ -26,7 +26,7 @@ https://github.com/Dark-Xploit/CypherX
 
 @${me.split("@")[0]}👋, Don't forget to star and fork my repository😊
 
-> 𝐗𝐩𝐥𝐨𝐚𝐝𝐞𝐫𝐁𝐨𝐭`;
+> global.wm`;
 
         Cypher.sendMessage(m.chat, { text: repos, contextInfo: {
           mentionedJid: [m.sender],
