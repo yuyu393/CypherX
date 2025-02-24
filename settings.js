c9f83ce1e6911fbe15df16c60a2b1e99696bd99d
Tylor <tylor.9.kid@gmail.com>
2025-02-24 11:21:46 +0300

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,4 +1,6 @@
-  //project_name : CypherX
+//-------------------[ BOT SETTINGS ]------------------// 
+
+// @project_name : CypherX
 // @author : TYLOR
 // @youtube : https://www.youtube.com/@heyits_tylor
 // @instagram : heyits_tylor
@@ -6,122 +8,139 @@
 // @github : Dark-Xploit
 // @tiktok : heyits_tylor
 // @whatsapp : +254754783972
-//*
-//* 
-//=================================================//
+
+//----------------------[ CYPHER-X ]----------------------//
+
 const fs = require('fs')
 const { color } = require('./lib/color')
 if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 
-//=================================================//
+//--------------------[ SESSION ID ]----------------------//
+
 global.SESSION_ID = process.env.SESSION_ID || '' 
 //Enter your Xploader session id here; must start with XPLOADER-BOT:~
 
-//=================================================//
+//--------------------[ BOT NAME ]----------------------//
+
 global.botname = process.env.BOT_NAME || 'CypherX' 
-//Your desired bot name
 
-//=================================================//
+//-----------------[ OWNER NUMBER ]------------------//
+
 global.ownernumber = process.env.OWNER_NUMBER || '254754783972' 
-//Type your main number here
 
-//=================================================//
+//--------------------[ SUDO ]--------------------------//
+
 global.sudo = process.env.SUDO ? process.env.SUDO.split(',') : ['254796180105', '254712345678'];
 // Type additional allowed users here
 //NB: They'll be able to use every functions of the bot without restrictions.
 
-//=================================================//
+//-----------------[ OWNER NAME ]------------------//
+
 global.ownername = process.env.OWNER_NAME || 'Tylor' 
-//Type your name here
 
-//=================================================//
-global.plink = process.env.PLINK || "https://www.instagram.com/heyits_tylor?igsh=YzljYTk1ODg3Zg=="
+//------------[ STICKER PACKNAME ]-----------------//
 
-//=================================================//
-global.wm = process.env.GL_WM || "©CypherX"
-
-//=================================================//
 global.packname = process.env.STICKER_PACK_NAME || "Cypher" 
-//The sticker pack name
 
-//=================================================//
+//--------------[ STICKER AUTHOR NAME ]------------//
+
 global.author = process.env.STICKER_AUTHOR_NAME || "X" 
-//The sticker author name
 
-//=================================================//
+//-------------------[ BOT'S PREFIX ]--------------------//
+
 global.prefixz = process.env.BOT_PREFIX || '.'
-//Set your desired prefix
 
-//=================================================//
+//-----------------[ BOT'S MODE ]-----------------------//
+
 global.mode = process.env.MODE || 'public';
 // Set 'private' to enable private mode
 // Set 'public' to enable public mode
 // Set 'group' to enable only group
 // Set 'pm' to enable only private chats
 
-//=================================================//
-global.statusemoji = process.env.STATUS_EMOJI || '🧡'
-//Enter the emoji that you wish to be reacted to statuses 
+//----------[ STATUS REACTION EMOJI ]--------------//
 
-//=================================================//
+global.statusemoji = process.env.STATUS_EMOJI || '🧡'
+
+//---------------[ AUTO VIEW STATUS ]---------------//
+
 global.autoviewstatus = process.env.AUTO_STATUS_VIEW || 'true'
 // set true to enable and false to disable auto status view
 
-//=================================================//
+//--------------[ AUTO REACT STATUS ]--------------//
+
 global.autoreactstatus = process.env.AUTO_STATUS_REACT || 'false'
 // set true to enable and false to disable auto status react
 
-//=================================================//
+//---------------[ ALWAYS ONLINE ]------------------//
+
 global.alwaysonline = process.env.ALWAYS_ONLINE || 'true'
 //Set true to make the bot online 24/7 or set false to disable always online
 
 
-//=================================================//
+//--------------------[ CHATBOT ]-----------------------//
+
 global.chatbot = process.env.CHATBOT || 'false'
 // set true to enable and false to disable auto ai chatbot
 
-//=================================================//
+//-------------------[ ANTI DELETE ]--------------------//
+
 global.antidelete = process.env.ANTIDELETE || 'private'
 // options:- 'private', 'chat' or 'off'
 // private = Sends to message yourself 
 // chat = sends to the current chat 
 // off = Disables detection of deleted messages
 
-//=================================================//
+//---------------------[ ANTI EDIT ]----------------------//
+
 global.antiedit = process.env.ANTI_EDIT || 'private'
 // options:- 'private', 'chat' or 'off'
 // private = Sends to message yourself 
 // chat = sends to the current chat 
 // off = Disables detection of edited messages
 
-//=================================================//
+//---------------------[ ANTI CALL ]----------------------//
+
 global.anticall = process.env.ANTI_CALL || 'false'
 // set true to enable and false to disable auto blocking of callers
 
-//=================================================//
+//---------------[ WELCOME MESSAGE ]----------------//
+
 global.welcome = process.env.WELCOME_MSG || 'false'
 // set true to enable and false to disable welcoming and left messages to groups upon joining or leaving groups
 
-//=================================================//
+//----------------------[ TIMEZONE ]--------------------//
+
 global.timezones = process.env.TIMEZONE || "Africa/Nairobi" 
 //Don't edit this if you don't know!
 
-//=================================================//
+//--------------------[ AUTO READ ]--------------------//
+
 global.autoread = process.env.AUTO_READ || 'false';
 // Set to 'true' to enable automatic reading of messages
 
-//=================================================//
+//-------------------[ MENU STYLE ]--------------------//
+
 global.menustyle = process.env.MENU_STYLE || '2' 
-// options 1, 2, 3, 4 or 5
+// options 1, 2, 3, 4, 5 or 6
 // 1 = Document menu(Android only)
 // 2 = Text only menu(Android & iOS)
 //3 = Image menu with context(Android & iOS)
 //4 = Image menu(Android & iOS)
-//5 = Payment menu
+//5 = Footer/faded menu
+//6 = Payment menu
 
-//=================================================//
-//Replies
+//-----------------[ CONTEXT LINK ]--------------------//
+
+global.plink = process.env.PLINK || "https://www.instagram.com/heyits_tylor?igsh=YzljYTk1ODg3Zg---"
+
+//------------------[ WATERMARK ]--------------------//
+
+global.wm = process.env.GL_WM || "©CypherX"
+
+//---------------------[ REPLIES ]-----------------------//
+
 global.mess = { 
   done: '*Done*', 
   success: '©CypherX', 
@@ -130,9 +149,9 @@ global.mess = {
   admin: '*You’ll unlock this feature with me as an admin!*', 
   notadmin: '*This feature will work once you become an admin. A way of ensuring order!*' 
 }
-//=================================================//
 
-//=================================================//
+//--------------------[ WATCHER ]-----------------------//
+
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
   fs.unwatchFile(file)
@@ -140,4 +159,5 @@ fs.watchFile(file, () => {
   delete require.cache[file]
   require(file)
 })
-//=================================================//
+
+//----------------------[ CYPHER-X ]----------------------//
